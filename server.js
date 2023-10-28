0d0777c2537a8e300c2ed75cfc2adebbd84b8c58
morris-hub <sebastiansulzbach@web.de>
2023-10-28 15:17:53 +0200

    made deployable for github pages

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 server.use(express.static('./public'));
 
-server.get('/', (req, res) => res.render('index'));
+server.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 server.get('/start', (req, res) => res.redirect('game.html'));
 server.get('/settings', (req, res) => res.redirect('settings.html'));
 server.get('/story', (req, res) => res.redirect('story.html'));
